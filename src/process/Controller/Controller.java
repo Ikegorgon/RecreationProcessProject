bc4313f9fefe7ed1a2d4c6c27434c813f132b4cc
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2017-09-07 13:52:59 -0600

    Added a test scanner

--- src/process/Controller/Controller.java
+++ src/process/Controller/Controller.java
@@ -1,8 +1,22 @@
 package process.Controller;
 
+import java.util.Scanner;
+
 public class Controller {
 	public void start() {
 		String x = "Hello, I have made this project.";
 		System.out.println(x);
+		
+		testScanner();
+	}
+	
+	private void testScanner() {
+		String x = "What is your favorite color?";
+		Scanner firstScanner;
+		firstScanner = new Scanner(System.in);
+		System.out.println(x);
+		String answer = firstScanner.next();
+		System.out.println("Oh cool, I like " + answer);
+		firstScanner.close();
 	}
 }
